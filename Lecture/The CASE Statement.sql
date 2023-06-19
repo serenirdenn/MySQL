2c2b6ac6fc97e5cf614ec11888fa62becaf1a159
Seren İrden <108147700+serenirdenn@users.noreply.github.com>
2023-06-19 23:11:21 +0300

    Update The CASE Statement.sql

--- Lecture/The CASE Statement.sql
+++ Lecture/The CASE Statement.sql
@@ -54,8 +54,7 @@ SELECT
     MAX(s.salary) - MIN(s.salary) AS salary_difference,
     CASE
         WHEN MAX(s.salary) - MIN(s.salary) > 30000 THEN 'Salary was raised by more than $30,000'
-        WHEN MAX(s.salary) - MIN(s.salary) BETWEEN 20000 AND 30000 THEN 
-									'Salary was raised by more than $20,000 but less than $30,000'
+        WHEN MAX(s.salary) - MIN(s.salary) BETWEEN 20000 AND 30000 THEN 'Salary was raised by more than $20,000 but less than $30,000'
         ELSE 'Salary was raised by less than $20,000'
     END AS salary_increase
 FROM
